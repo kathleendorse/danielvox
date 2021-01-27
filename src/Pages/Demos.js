439c4ac3094e26ddeaca8aa2a754d2396ceee2ee
Kathleen Dorse <kathleen.dorse@gmail.com>
2021-01-27 16:31:52 -0600

    audiobook update + alternate demos view

--- src/Pages/Demos.js
+++ src/Pages/Demos.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import {Container, Row, Col, Table} from "react-bootstrap";
+import {Container, Row, Col, Table, ButtonGroup, DropdownButton, Button, Dropdown} from "react-bootstrap";
 import ResponsivePlayer from "../Components/ResponsivePlayer";
 
 
@@ -66,56 +66,77 @@ function Demos(){
                 </Col>
                 <Col></Col>
             </Row> 
-            <Row><br></br></Row>   
+            <Row><br></br></Row>  
+{/* ----------testOPTION 1 BUTTON DROP DOWN CENTERED PLAYER---------- */}
+            <Row>
+                <Col></Col>
+                <Col xs={10}>
+                    <ButtonGroup>
+                        <DropdownButton as={ButtonGroup} title="Select Title to Listen" id="bg-nested-dropdown" size="lg">
+                            {demos.map((demo)=>(
+                                <Dropdown.Item eventKey={demo.id} key={demo.id} onClick={()=>{setUrl(demo.link)}}>
+                                    <h5 className="text-light">
+                                        {demo.target}
+                                        </h5>
+                                </Dropdown.Item>
+                            ))}
+                        </DropdownButton>
+                    </ButtonGroup>
+                    <br></br>
+                    <br></br>
+                </Col>
+                <Col></Col>
+            </Row> 
+            <Row>
+                <Col></Col>
+                <Col xs={8}>
+                    <ResponsivePlayer url={url}/>  
+                </Col>
+                <Col></Col>
+
+            </Row>
+{/* ------------------------------------------------------------------ */}
+
+
+            <Row>
+                <Col>
+                <p>------------------------------------------------------------------</p>
+                <br></br>
+                <br></br>
+                <br></br>
+                </Col>
+            </Row>
+{/* ----------OPTION 2 TABLE & HALF SIZE PLAYER---------- */}
+
             <Row>
                 <Col></Col>
                 <Col xs={5}>
                     <ResponsivePlayer url={url}/>
                 </Col>                
                 <Col xs={5}>
-
                     <Table striped bordered hover responsive="sm" size="sm" >
-                        {/* <thead>
-                            <tr>
-                            <th>Target</th>
-                            <th>Tags</th>
-                            <th>Link</th>
-
-                            </tr>
-                        </thead> */}
                         <tbody>
-                            {/* <tr id="commerical-demos" onClick={() => handleClick()}> */}
-                            {/* <button onClick={() => this.handleClick()}>
-        Click me
-      </button> */}
-                            {/* <td>Commercial</td>
-                            <td>#WorkingClass #Friendly</td>
-                            <td>@mdo</td>
-                            </tr> */}
-                            {/* <tr>
-                            <td>2</td>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                            </tr>
-                            <tr>
-                            <td>3</td>
-                            <td colSpan="2">Larry the Bird</td>
-                            <td>@twitter</td>
-                            </tr> */}
                             {demos.map((demo)=>(
                                 <tr key={demo.id} onClick={()=>{setUrl(demo.link)}} className="table-dark">
                                     <td>{demo.target}</td>
                                     <td>{demo.tags}</td>
-                                    {/* <td>
-                                        <a href={demo.link} target="_blank"  rel="noreferrer noopener" aria-label="">
-                                            <img src={iconSoundcloud} alt="soundcloud"/>
-                                        </a>
-                                    </td> */}
+
                                 </tr>
                             ))}
                         </tbody>
                     </Table>
+                        {/* <div className="btn-group btn-group-toggle" data-toggle="buttons">
+                            <label className="btn btn-primary active">
+                                <input type="radio" name="options" id="option1" autocomplete="off" checked=""> Active
+                            </label>
+                            <label className="btn btn-primary">
+                                <input type="radio" name="options" id="option2" autocomplete="off"> Radio
+                            </label>
+                            <label className="btn btn-primary">
+                                <input type="radio" name="options" id="option3" autocomplete="off"> Radio
+                            </label>
+                        </div> */}
+
                 </Col>
                 <Col></Col>
             </Row>
