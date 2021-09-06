148cb4c08f071790a06c7ea824b979d6279f0208
Kathleen Dorse <kathleen.dorse@gmail.com>
2021-09-06 16:10:23 -0500

    removed unused container component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import './App.css';
 import TopNav from "./Components/Nav";
 import Sections from "./Pages/Test";
-import {Container} from 'react-bootstrap';
 
 function App(){
   return (
