745dbf7e81cec54568f78a2d40bbfcc0ff159c17
Kathleen Dorse <kathleen.dorse@gmail.com>
2021-09-06 16:10:49 -0500

    new nav color

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -16,7 +16,7 @@ const TopNav= props => {
   
 
     return(
-        <nav id="sticky" className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+        <nav id="sticky" className="navbar navbar-expand-lg navbar-dark bg-primary sticky-top">
             <div className="container-fluid"> 
                 <a className="navbar-brand" href="#page-top">danielvox</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded={!isNavCollapsed ? true : false} aria-label="Toggle navigation" onClick={handleNavCollapse}>
